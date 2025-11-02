8aa798e99e20a912b59b6cd3957fcfbfb24d8000
Waliyullah <waliyullahadewale30@gmail.com>
2025-11-02 20:05:55 +0100

    changed  sync def

--- input_agent/main.py
+++ input_agent/main.py
@@ -11,11 +11,11 @@ class TextPayload(BaseModel):
 async def transform_text(payload: TextPayload):
     async with httpx.AsyncClient() as client:
         # Capitalizer
-        cap_res = await client.post("http://capitalizer_agent:8001/capitalize/", json=payload.dict())
+        cap_res = await client.post("http://localhost:8001/capitalize/", json=payload.dict())
         capitalized = cap_res.json()["text"]
 
         # Reverse
-        rev_res = await client.post("http://reverse_agent:8002/reverse/", json={"text": capitalized})
+        rev_res = await client.post("http://localhost:8002/reverse/", json={"text": capitalized})
         reversed_text = rev_res.json()["text"]
 
     return {"text": reversed_text}
